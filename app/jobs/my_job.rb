0ab5f19c6d8c4d7817b2c8a4c470516d607ed73c
Enrique Arias <emoriarty81@gmail.com>
2024-03-18 15:22:53 +0100

    remove useless method call

--- app/jobs/my_job.rb
+++ app/jobs/my_job.rb
@@ -2,6 +2,5 @@ class MyJob < ApplicationJob
   def perform(*args)
     # Do something later
     puts "I am in the job: #{args.join(", ")}"
-    finish # This ensures that the job is kept in the database
   end
 end

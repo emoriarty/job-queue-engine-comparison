5231977be8486db94527461b90925ea99d246af5
Enrique Arias <emoriarty81@gmail.com>
2024-03-24 09:20:15 +0100

    adapt script for solid_queue

--- bin/last_batch_duration.rb
+++ bin/last_batch_duration.rb
@@ -5,14 +5,13 @@ puts RUBY_DESCRIPTION
 APP_PATH = File.expand_path("../config/application", __dir__)
 require_relative "../config/boot"
 require APP_PATH # require application.rb
-require "que/active_record/model"
 
 Rails.application.require_environment!
 
 puts "Rails app loaded!"
 
-first_job = Que::ActiveRecord::Model.all.order(finished_at: :asc).select(:finished_at).first
-last_job = Que::ActiveRecord::Model.all.order(finished_at: :asc).select(:finished_at).last
+first_job = SolidQueue::Job.all.order(finished_at: :asc).select(:finished_at).first
+last_job = SolidQueue::Job.all.order(finished_at: :asc).select(:finished_at).last
 seconds = (last_job.finished_at - first_job.finished_at) % 60
 minutes = ((last_job.finished_at - first_job.finished_at) / 60) % 60
 hours = (((last_job.finished_at - first_job.finished_at) / 60) /60) % 60

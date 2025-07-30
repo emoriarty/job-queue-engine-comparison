abce5fdcd04b0e0de9844bae8d9c240a377a1170
Enrique Arias <emoriarty81@gmail.com>
2025-07-30 09:58:33 +0200

    refactor job benchmark creation

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -5,11 +5,6 @@ class ApplicationJob < ActiveJob::Base
   # Most jobs are safe to ignore if the underlying records are no longer available
   # discard_on ActiveJob::DeserializationError
 
-  after_enqueue do |job|
-    job_id = job.arguments.first
-    JobBenchmark.create!(job_id:, job_type:, queued_at: Time.now)
-  end
-
   around_perform do |job, block|
     started_at = Time.now
     block.call

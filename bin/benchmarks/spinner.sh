65b2bbe932e177ff35bff581332e19f66e899ad9
Enrique Arias <emoriarty81@gmail.com>
2025-08-20 16:32:11 +0200

    refactor + gini metrics

--- bin/benchmarks/spinner.sh
+++ bin/benchmarks/spinner.sh
@@ -1,4 +1,4 @@
-function show_spinner_until_jobs_complete() {
+function display_progress_until_jobs_complete() {
   local spinner='|/-\'
   local delay=0.1
   local i=0
@@ -6,8 +6,8 @@ function show_spinner_until_jobs_complete() {
 
   tput civis  # Hide cursor
   while true; do
-    #local count=$(bin/rails runner "puts Que.execute('SELECT COUNT(*) FROM que_jobs WHERE finished_at IS NOT NULL').last[:count]" 2>/dev/null)
-    local count=$(bin/rails runner "puts JobBenchmark.where.not(finished_at: nil).count" 2>/dev/null)
+    local count
+    count=$(bin/rails runner "puts JobBenchmark.where.not(finished_at: nil).count" 2>/dev/null || echo 0)
 
     if [[ "$count" -ge "$jobs_count" ]]; then
       break
@@ -18,7 +18,6 @@ function show_spinner_until_jobs_complete() {
     printf "\rWaiting for jobs to finish... %s (Pending jobs: %s)       " "${spinner:$i:1}" "$pending"
     sleep "$delay"
   done
-  printf "\rAll jobs completed.                                                  "
   tput cnorm  # Show cursor
 }
 
